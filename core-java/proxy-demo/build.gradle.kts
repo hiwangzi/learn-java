f120938cc8d4a40e2306725525c8057e89bee5cb
Zi Wang <me@hiwangzi.com>
2024-09-05 12:35:38 +0800

    feat(core-java): upgrade lockback and define children dependencies in core-java

--- core-java/proxy-demo/build.gradle.kts
+++ core-java/proxy-demo/build.gradle.kts
@@ -1,14 +1,4 @@
-description = "core-java.proxy-demo"
-
 plugins {
     id("buildlogic.java-conventions")
 }
 
-dependencies {
-    implementation(libs.slf4j.api)
-    implementation(libs.logback.classic)
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
